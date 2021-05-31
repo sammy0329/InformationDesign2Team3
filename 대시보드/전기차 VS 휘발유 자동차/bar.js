85e85efea0452532181218ea53d4ba8c7cbc35c8
nokcharathae <64080938+nokcharathae@users.noreply.github.com>
2021-05-31 21:51:47 +0900

    연습

--- 대시보드/전기차 VS 휘발유 자동차/bar.js
+++ 대시보드/전기차 VS 휘발유 자동차/bar.js
@@ -32,43 +32,43 @@ bar();
 console.log(Ice);
 console.log(Ev);
 //----- 전기차 vs 휘발유차 ----
-var canvas = document.getElementById("Canvas");
-var ctx = canvas.getContext("2d"); // 캔버스 객체 생성
+// var canvas = document.getElementById("Canvas");
+// var ctx = canvas.getContext("2d"); // 캔버스 객체 생성
 
-var rectX = 30;
-var rectY = 30;
-var rectWidth = 10;
-var rectHeight = 40;
-var cornerRadius = 10;
+// var rectX = 30;
+// var rectY = 30;
+// var rectWidth = 10;
+// var rectHeight = 40;
+// var cornerRadius = 10;
 
-// Set faux rounded corners
-ctx.lineJoin = "round";
-ctx.lineWidth = cornerRadius;
+// // Set faux rounded corners
+// ctx.lineJoin = "round";
+// ctx.lineWidth = cornerRadius;
 
-ctx.beginPath();
-// 색 설정
-ctx.strokeStyle = '#A9C9F7'; // 선 색
-ctx.fillStyle = '#333333'; // 채운 사각형 색
+// ctx.beginPath();
+// // 색 설정
+// ctx.strokeStyle = '#A9C9F7'; // 선 색
+// ctx.fillStyle = '#333333'; // 채운 사각형 색
 
-// 그리기
-ctx.strokeRect(rectX+(cornerRadius/2), rectY+(cornerRadius/2), rectWidth-cornerRadius, rectHeight-cornerRadius);
-ctx.fillRect(rectX+(cornerRadius/2), rectY+(cornerRadius/2), rectWidth-cornerRadius, rectHeight-cornerRadius);
-ctx.font = '30px Arial';
-ctx.fillText('전기차 vs 휘발유 자동차', rectX+15, rectY+30);
-ctx.font = '15px Arial';
-ctx.fillText('연료값', rectX+15, rectY+75);
-ctx.fillText('완속 : 1kWh', rectX+15, rectY+110);
-ctx.fillText('급속 : 1kWh', rectX+15, rectY+140);
-var img = new Image();
-var img1 = new Image();
-img.src = "EV.png";
-img.onload = function(e){
-  ctx.drawImage(img, 45,190, 90,50);
-}
-img1.src = "ICE.png";
-img1.onload = function(e){
-  ctx.drawImage(img1, 45,290, 90,50);
-}
+// // 그리기
+// ctx.strokeRect(rectX+(cornerRadius/2), rectY+(cornerRadius/2), rectWidth-cornerRadius, rectHeight-cornerRadius);
+// ctx.fillRect(rectX+(cornerRadius/2), rectY+(cornerRadius/2), rectWidth-cornerRadius, rectHeight-cornerRadius);
+// ctx.font = '30px Arial';
+// ctx.fillText('전기차 vs 휘발유 자동차', rectX+15, rectY+30);
+// ctx.font = '15px Arial';
+// ctx.fillText('연료값', rectX+15, rectY+75);
+// ctx.fillText('완속 : 1kWh', rectX+15, rectY+110);
+// ctx.fillText('급속 : 1kWh', rectX+15, rectY+140);
+// var img = new Image();
+// var img1 = new Image();
+// img.src = "EV.png";
+// img.onload = function(e){
+//   ctx.drawImage(img, 45,190, 90,50);
+// }
+// img1.src = "ICE.png";
+// img1.onload = function(e){
+//   ctx.drawImage(img1, 45,290, 90,50);
+// }
 
 //---- 막대 그래프 ----
 // 상하좌우 여백 수치. 하단에는 축이 그려져야 하니까 여백을 많이.
