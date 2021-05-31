b548098fc00753470f630c0e25e5591899500f7a
sammy0329 <sammy0329@naver.com>
2021-05-31 14:45:14 +0900

    선택시 차종변경 라인색상 grid추가

--- 대시보드/연습용/pr4.js
+++ 대시보드/연습용/pr4.js
@@ -2,14 +2,14 @@ CarValue=0 //자동차 인덱스 전역변수
 CarText="코나" //자동차 모델명 전역변수
 ConditionValue=0
 ConditionText="가격"
-CityText="강릉"
+CityText=""
 
-//처음화면에 승차인원 부터 띄워줌
+//처음화면에 가격 부터 띄워줌
 Make_Ev_Compared_Chart();
 
 //차량 선택에 따른 value 값 및 text 저장 함수
 function changeLangSelect(){ 
-    var CarSelect = document.getElementById("name"); 
+    let CarSelect = document.getElementById("name"); 
     // select element에서 선택된 option의 value가 저장된다. 
     CarValue = CarSelect.options[CarSelect.selectedIndex].value; 
     // select element에서 선택된 option의 text가 저장된다. 
@@ -20,17 +20,24 @@ function changeLangSelect(){
 
 //조건 선택에 따른 value 값 및 text 저장 함수
 function changeConditionSelect(){ 
-    var ConditionSelect = document.getElementById("condition"); 
+    let ConditionSelect = document.getElementById("condition"); 
     // // select element에서 선택된 option의 value가 저장된다. 
      ConditionValue = ConditionSelect.options[ConditionSelect.selectedIndex].value;
     // select element에서 선택된 option의 text가 저장된다. 
      ConditionText = ConditionSelect.options[ConditionSelect.selectedIndex].text;
+     if(ConditionText==="보조금"){
+        document.all.region.style.visibility="visible";
+        document.all.city.style.visibility="visible";
+     }else{
+        document.all.region.style.visibility="hidden";
+        document.all.city.style.visibility="hidden";
+     }
      Make_Ev_Compared_Chart(); 
-    console.log(ConditionText)}
+   }
 
 //city 값을 저장하는 함수
 function changeCitySelect(){ 
-    var CitySelect = document.getElementById("city"); 
+    let CitySelect = document.getElementById("city"); 
     // // select element에서 선택된 option의 value가 저장된다. 
     //  CityValue = CitySelect.options[CitySelect.selectedIndex].value;
     // select element에서 선택된 option의 text가 저장된다. 
@@ -41,55 +48,54 @@ function changeCitySelect(){
 
 //지역 선택시 세부내역인 구? 선택 사항 변경 함수
 function categoryChange(e) {
-    var city_a = ["세부지역을 선택해주세요","강릉시", "고성군", "동해시", "삼척시","속초시","양구군","양양군","영월군","원주시","인제군","정선군","철원군","춘천시","태백시","평창군","홍천군","화천군","횡성군"];
-    var city_b = ["세부지역을 선택해주세요","가평군", "고양시", "과천시", "광명시","광주시","구리시","군포시","김포시","남양주시","동두천시","부천시","성남시","수원시","시흥시","안산시","안성시","안양시","양주시","양평군","여주시","연천군","오산시","용인시","의왕시","의정부시","이천시","파주시","평택시","포천시","하남시","화성시"];
-    var city_c = ["세부지역을 선택해주세요","거제시", "거창군", "고성군", "김해시", "남해군","밀양시","사천시","산청군","양산시","의령군","진주시","창녕군","창원시","통영시","하동군","함안군","함양군","합천군"];
-    var city_d = ["세부지역을 선택해주세요","경산시", "경주시", "고령군", "구미시", "김천시","문경시","봉화군","상주시","성주군","안동시","영덕군","영양군","영주시","영천시","예천군","울릉군","울진군","청도군","청송군","칠곡군","포항시"];
-    var city_e = ["세부지역을 선택해주세요","광주광역시"];
-    var city_f = ["세부지역을 선택해주세요","대구광역시"];
-    var city_g = ["세부지역을 선택해주세요","대전광역시"];
-    var city_h = ["세부지역을 선택해주세요","부산광역시"];
-    var city_i = ["세부지역을 선택해주세요","서울특별시"];
-    var city_j = ["세부지역을 선택해주세요","세종특벽시"];
-    var city_k = ["세부지역을 선택해주세요","울산광역시"];
-    var city_l = ["세부지역을 선택해주세요","인천광역시"];
-    var city_m = ["세부지역을 선택해주세요","강진군", "고흥군", "곡성군", "광양시", "구례군","나주시","담양군","목포시","무안군","보성군","순천시","신안군","여수시","영광군","영암군","완도군","장성군","장흥군","진도군","함평군","해남군","화순군"];
-    var city_n = ["세부지역을 선택해주세요","고창군", "군산시", "김제시", "남원시", "무주군","부안군","순창군","완주군","익산시","임실군","장수군","전주시","정읍시","진안군"];
-    var city_o = ["세부지역을 선택해주세요","제주특별시"];
-    var city_p = ["세부지역을 선택해주세요","계룡시", "공주시", "금산군", "논산시", "당진시","보령시","부여군","서산시","서천군","아산시","예산군","천안시","청양군","태안군","홍성군"];
-    var city_q = ["세부지역을 선택해주세요","괴산군", "단양군", "보은군", "영동군", "옥천군","음성군","제천시","증평군","진천군","청주시","충주시"];
+    let city_a = ["세부지역을 선택해주세요","강릉시", "고성군", "동해시", "삼척시","속초시","양구군","양양군","영월군","원주시","인제군","정선군","철원군","춘천시","태백시","평창군","홍천군","화천군","횡성군"];
+    let city_b = ["세부지역을 선택해주세요","가평군", "고양시", "과천시", "광명시","광주시","구리시","군포시","김포시","남양주시","동두천시","부천시","성남시","수원시","시흥시","안산시","안성시","안양시","양주시","양평군","여주시","연천군","오산시","용인시","의왕시","의정부시","이천시","파주시","평택시","포천시","하남시","화성시"];
+    let city_c = ["세부지역을 선택해주세요","거제시", "거창군", "고성군", "김해시", "남해군","밀양시","사천시","산청군","양산시","의령군","진주시","창녕군","창원시","통영시","하동군","함안군","함양군","합천군"];
+    let city_d = ["세부지역을 선택해주세요","경산시", "경주시", "고령군", "구미시", "김천시","문경시","봉화군","상주시","성주군","안동시","영덕군","영양군","영주시","영천시","예천군","울릉군","울진군","청도군","청송군","칠곡군","포항시"];
+    let city_e = ["세부지역을 선택해주세요","광주광역시"];
+    let city_f = ["세부지역을 선택해주세요","대구광역시"];
+    let city_g = ["세부지역을 선택해주세요","대전광역시"];
+    let city_h = ["세부지역을 선택해주세요","부산광역시"];
+    let city_i = ["세부지역을 선택해주세요","서울특별시"];
+    let city_j = ["세부지역을 선택해주세요","세종특벽시"];
+    let city_k = ["세부지역을 선택해주세요","울산광역시"];
+    let city_l = ["세부지역을 선택해주세요","인천광역시"];
+    let city_m = ["세부지역을 선택해주세요","강진군", "고흥군", "곡성군", "광양시", "구례군","나주시","담양군","목포시","무안군","보성군","순천시","신안군","여수시","영광군","영암군","완도군","장성군","장흥군","진도군","함평군","해남군","화순군"];
+    let city_n = ["세부지역을 선택해주세요","고창군", "군산시", "김제시", "남원시", "무주군","부안군","순창군","완주군","익산시","임실군","장수군","전주시","정읍시","진안군"];
+    let city_o = ["세부지역을 선택해주세요","제주특별시"];
+    let city_p = ["세부지역을 선택해주세요","계룡시", "공주시", "금산군", "논산시", "당진시","보령시","부여군","서산시","서천군","아산시","예산군","천안시","청양군","태안군","홍성군"];
+    let city_q = ["세부지역을 선택해주세요","괴산군", "단양군", "보은군", "영동군", "옥천군","음성군","제천시","증평군","진천군","청주시","충주시"];
         
     var target = document.getElementById("city");
-       
-    if(e.value == "a") var d = city_a;
-    else if(e.value == "b") var d = city_b;
-    else if(e.value == "c") var d = city_c;
-    else if(e.value == "d") var d = city_d;
-    else if(e.value == "e") var d = city_e;
-    else if(e.value == "f") var d = city_f;
-    else if(e.value == "g") var d = city_g;
-    else if(e.value == "h") var d = city_h;
-    else if(e.value == "i") var d = city_i;
-    else if(e.value == "j") var d = city_j;
-    else if(e.value == "k") var d = city_k;
-    else if(e.value == "l") var d = city_l;
-    else if(e.value == "m") var d = city_m;
-    else if(e.value == "n") var d = city_n;
-    else if(e.value == "o") var d = city_o;
-    else if(e.value == "p") var d = city_p;
-    else if(e.value == "q") var d = city_q;
+
+    let d;
+    if(e.value == "a")  d = city_a;
+    else if(e.value == "b")  d = city_b;
+    else if(e.value == "c")  d = city_c;
+    else if(e.value == "d")  d = city_d;
+    else if(e.value == "e")  d = city_e;
+    else if(e.value == "f")  d = city_f;
+    else if(e.value == "g")  d = city_g;
+    else if(e.value == "h")  d = city_h;
+    else if(e.value == "i")  d = city_i;
+    else if(e.value == "j")  d = city_j;
+    else if(e.value == "k")  d = city_k;
+    else if(e.value == "l")  d = city_l;
+    else if(e.value == "m")  d = city_m;
+    else if(e.value == "n")  d = city_n;
+    else if(e.value == "o")  d = city_o;
+    else if(e.value == "p")  d = city_p;
+    else if(e.value == "q")  d = city_q;
     target.options.length = 0;
        
     for (x in d) {
-        var opt = document.createElement("option");
+        let opt = document.createElement("option");
         opt.value = d[x];
         opt.innerHTML = d[x];
         target.appendChild(opt);
-        } 
-    
+        }    
       }
       
-
 //내가 선택한 차종 색 변경
 function update_color(){
     d3.selectAll(".bar")
@@ -105,9 +111,9 @@ function Make_Ev_Compared_Chart(){
 if(ConditionText==="보조금"){
 
     // set the dimensions and margins of the graph
-var margin = {top: 20, right: 30, bottom: 40, left: 140},
+let margin = {top: 40, right: 20, bottom: 40, left: 140},
 width = 460 - margin.left - margin.right,
-height = 500 - margin.top - margin.bottom;
+height = 550 - margin.top - margin.bottom;
 
 //div 삭제 후 다시 만들어주면서 그래프 갱신
 const Olddiv = document.getElementById("my_dataviz");
@@ -117,7 +123,7 @@ NewDiv.setAttribute("id", "my_dataviz");
 document.body.appendChild(NewDiv);
 
 // append the svg object to the body of the page
-var svg = d3.select("#my_dataviz")
+let svg = d3.select("#my_dataviz")
 .append("svg")
 .attr("width", width + margin.left + margin.right)
 .attr("height", height + margin.top + margin.bottom)
@@ -126,7 +132,7 @@ var svg = d3.select("#my_dataviz")
       "translate(" + margin.left + "," + margin.top + ")");
 
 d3.json("Subsidy2.json",function(error,data){
-var dataset = data.Subsidy;
+let dataset = data.Subsidy;
 //filter로 지역에 해당하는 값만 추출
 let result=dataset.filter(x=>{
     return x.지역구분===CityText
@@ -137,19 +143,22 @@ result.sort(function(b, a) {
   });
 
 //x axis 조절
-var x = d3.scaleLinear()
+let x = d3.scaleLinear()
     .domain([0, d3.max(result, function(d){ return d['보조금'];})])
-    .range([ 5, width]);
+    .range([ 0, width]);
 
 svg.append("g")
     .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x))
+    .attr("class", "grid")
+    
+    .call(d3.axisBottom(x).ticks(5).tickSize(-height))
+    
     .selectAll("text")
     .attr("transform", "translate(-10,0)rotate(-45)")
     .style("text-anchor", "end");
 
 // Y axis 조절
-var y = d3.scaleBand()
+let y = d3.scaleBand()
     .range([ 0, height ])
     .domain(result.map(function(d) { return d.차종별; }))
     .padding(.3);
@@ -157,39 +166,6 @@ var y = d3.scaleBand()
 svg.append("g")
     .call(d3.axisLeft(y))
 
-// create a tooltip
-var Tooltip = d3.select("#my_dataviz")
-.append("div")
-.style("opacity", 0)
-.attr("class", "tooltip")
-.style("background-color", "white")
-.style("border", "solid")
-.style("border-width", "2px")
-.style("border-radius", "5px")
-.style("padding", "5px")
-
-// Three function that change the tooltip when user hover / move / leave a cell
-var mouseover = function(d) {
-Tooltip
-  .style("opacity", 1)
-d3.select(this)
-  .style("stroke", "black")
-  .style("opacity", 1)
-  .html(d[ConditionText])
-}
-var mousemove = function(d) {
-Tooltip
-  .html(d[ConditionText])
-  .style("left", (d3.mouse(this)[0]-70) + "px")
-  .style("top", (d3.mouse(this)[1]) + "px")
-}
-var mouseleave = function(d) {
-Tooltip
-  .style("opacity", 0)
-d3.select(this)
-  .style("stroke", "none")
-  .style("opacity", 0.8)
-}
 
 svg.selectAll("myRect")
     .data(result)
@@ -202,19 +178,33 @@ svg.selectAll("myRect")
     // .duration(2000)
     .attr("width", function(d) { return x(d['보조금']); })
     .attr("height", y.bandwidth() )
-    .on("mouseover",mouseover)
-    .on("mousemove", mousemove)
-    .on("mouseleave", mouseleave)
+    //mouserover 효과주기
+    .on("mouseover", function() { tooltip.style("display", null); })
+    .on("mouseout",  function() { tooltip.style("display", "none"); })
+    .on("mousemove", function(d) {
+        tooltip.style("left", (d3.event.pageX + 10) + "px");
+        tooltip.style("top", (d3.event.pageY - 10) + "px");
+        tooltip.text(d[ConditionText]+" 원"); 
+        })
+    .on("click",function(point, event) {
+        if(event.length <= 0) return;
+            CarText =point.차종별
+            $('#name').val(CarText).prop("selected",true);
+            update_color();  
+            });  
 
-  
+    let tooltip = d3.select("body").append("div")
+        .attr("class", "toolTip")
+        .style("display", "none");  
+        
 update_color();  
 
 });
 }else{
 // set the dimensions and margins of the graph
-var margin = {top: 20, right: 30, bottom: 40, left: 140},
-    width = 460 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+let margin = {top: 40, right: 20, bottom: 40, left: 140},
+width = 460 - margin.left - margin.right,
+height = 550 - margin.top - margin.bottom;
 
 //div 삭제 후 다시 만들어주면서 그래프 갱신
 const Olddiv = document.getElementById("my_dataviz");
@@ -224,7 +214,7 @@ NewDiv.setAttribute("id", "my_dataviz");
 document.body.appendChild(NewDiv);
 
 // append the svg object to the body of the page
-var svg = d3.select("#my_dataviz")
+let svg = d3.select("#my_dataviz")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -233,67 +223,88 @@ var svg = d3.select("#my_dataviz")
           "translate(" + margin.left + "," + margin.top + ")");
 
 d3.json("Compared_ev.json",function(error,data){
-    var dataset = data.cars;
+    let dataset = data.cars;
     //sorting
     dataset.sort(function(b, a) {
         return a[ConditionText] - b[ConditionText];
       });
  
 //x axis 조절
-    var x = d3.scaleLinear()
+    let x
+    if(ConditionText==="승차인원"){
+    x = d3.scaleLinear()
         .domain([0, d3.max(dataset, function(d){ return d[ConditionText];})])
-        .range([ 5, width]);
-
-    svg.append("g")
+        .range([ 0, width]);
+    
+        svg.append("g")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x))
+        .attr("class", "grid")
+        .call(d3.axisBottom(x).ticks(5).tickSize(-height))
+       
+
+    }else if(ConditionText==="가격"){
+      x = d3.scaleLinear()
+      .domain([0, d3.max(dataset, function(d){ return d[ConditionText];})])
+      .range([ 0, width]);
+
+      svg.append("g")
+        .attr("class", "grid")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x).ticks(5).tickSize(-height))
         .selectAll("text")
         .attr("transform", "translate(-10,0)rotate(-45)")
         .style("text-anchor", "end");
+        
+        // let y =100
+        // svg.append('line')
+        // .attr('x1', 0)
+        // .attr('y1', y)
+        // .attr('x2', width)
+        // .attr('y2', y)
+        // .attr('stroke', 'red')
+        
+    }else{
+        x = d3.scaleLinear()
+      .domain([0, d3.max(dataset, function(d){ return d[ConditionText];})])
+      .range([ 0, width]);
+
+      svg.append("g")
+        .attr("class", "grid")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x).ticks(5).tickSize(-height))
+          
+    //    svg.append("g")			
+    //   .attr("class", "grid")
+    //   .attr("transform", "translate(0," + height + ")")
+    //   .call(make_x_gridlines()
+    //       .tickSize(-height)
+    //       .tickFormat("")
+    //   )
+    }
 
 // Y axis 조절
-    var y = d3.scaleBand()
+    let y = d3.scaleBand()
         .range([ 0, height ])
         .domain(dataset.map(function(d) { return d.차종별; }))
         .padding(.3);
         
     svg.append("g")
         .call(d3.axisLeft(y))
+    
 
-// create a tooltip
-var Tooltip = d3.select("#my_dataviz")
-.append("div")
-.style("opacity", 0)
-.attr("class", "tooltip")
-.style("background-color", "white")
-.style("border", "solid")
-.style("border-width", "2px")
-.style("border-radius", "5px")
-.style("padding", "5px")
+let Unit;
+//단위 만들어서 합쳐보고 싶었는데...
+    if(ConditionText==="가격"){
+        Unit =" 원"
+    }else if(ConditionText==="최고속도" || ConditionText==="주행거리"){
+        Unit=" Km"
+    }else if(ConditionText==="연비"){
+        Unit=" kWh/km"
+    }else if(ConditionText==="승차인원"){
+        Unit=" 명"
+    }
 
-// Three function that change the tooltip when user hover / move / leave a cell
-var mouseover = function(d) {
-Tooltip
-  .style("opacity", 1)
-d3.select(this)
-  .style("stroke", "black")
-  .style("opacity", 1)
-//   .html(d[ConditionText])
-}
-var mousemove = function(d) {
-Tooltip
-  .html(d[ConditionText])
-  .style("left", (d3.mouse(this)[0]-70) + "px")  
-  .style("top", (d3.mouse(this)[0]) + "px")
-}
-var mouseleave = function(d) {
-Tooltip
-  .style("opacity", 0)
-d3.select(this)
-  .style("stroke", "none")
-  .style("opacity", 0.8)
-}
-
+    
     svg.selectAll("myRect")
         .data(dataset)
         .enter()
@@ -305,12 +316,47 @@ d3.select(this)
         // .duration(2000)
         .attr("width", function(d) { return x(d[ConditionText]); })
         .attr("height", y.bandwidth() )   
-        .on("mouseover",mouseover)
-        .on("mousemove", mousemove)
-        .on("mouseleave", mouseleave)
+        .on("mouseover", function() { tooltip.style("display", null); })
+        .on("mouseout",  function() { tooltip.style("display", "none"); })
+        .on("mousemove", function(d) {
+            tooltip.style("left", (d3.event.pageX + 10) + "px");
+            tooltip.style("top", (d3.event.pageY - 10) + "px");
+            tooltip.text(String(d[ConditionText])+Unit)
+        })
+        
+        .on("click",function(point, event) {
+            if(event.length <= 0) return;
+                CarText =point.차종별
+                $('#name').val(CarText).prop("selected",true);
+                update_color();  
+            });
 
         
-update_color();  
-    
+        // var line = d3.line()
+        // .x(function(d) {return xScale(d.x); })
+        // .y(function(d) {return yScale(d.y); });
+  
+        // svg.append("myRect")
+        //     .data()
+        //     .attr("fill", "none")
+        //     .attr("stroke", "blue")
+        //     .attr("stroke-width", "1.5px")
+        //     .attr("d", line);
+
+        // svg.append("path")
+        // .datum(data)
+        // .attr("fill", "none")
+        // .attr("stroke", "#69b3a2")
+        // .attr("stroke-width", 1.5)
+        // .attr("d", d3.line()
+        //   .y(function(d) { return y(d.연비) })
+        //   )
+
+
+        var tooltip = d3.select("body").append("div")
+        .attr("class", "toolTip")
+        .style("display", "none");
+    update_color();  
+
 });}
 }
